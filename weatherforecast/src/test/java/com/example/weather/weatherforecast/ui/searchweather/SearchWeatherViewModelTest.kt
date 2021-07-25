0acb0c1f618bf308f54a8d0aeab055acd92bcd45
Dai Doan <doanvandai2012@gmail.com>
2021-07-25 22:26:21 +0700

    update dependency

--- weatherforecast/src/test/java/com/example/weather/weatherforecast/ui/searchweather/SearchWeatherViewModelTest.kt
+++ weatherforecast/src/test/java/com/example/weather/weatherforecast/ui/searchweather/SearchWeatherViewModelTest.kt
@@ -1,8 +1,10 @@
 package com.example.weather.weatherforecast.ui.searchweather
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.weather.common.ui.CommonViewState
+import com.example.weather.domain.interaction.searchweather.GetKeySearchLengthUseCase
 import com.example.weather.domain.interaction.searchweather.SearchWeatherInfoUseCase
-import com.example.weather.domain.model.WeatherElement
+import com.example.weather.domain.interaction.searchweather.WeatherResultElement
 import com.example.weather.weatherforecast.util.MainCoroutineRule
 import com.example.weather.weatherforecast.util.getOrAwaitValue
 import io.mockk.coEvery
@@ -30,25 +32,46 @@ class SearchWeatherViewModelTest {
     var mainCoroutineRule = MainCoroutineRule()
 
     private val searchWeatherUseCase: SearchWeatherInfoUseCase = mockk()
+    private val getKeySearchLengthUseCase: GetKeySearchLengthUseCase = mockk()
     private lateinit var viewModel :SearchWeatherViewModel
 
     @Before
     fun setup() {
-        viewModel = SearchWeatherViewModel(searchWeatherUseCase)
+        coEvery { getKeySearchLengthUseCase() } returns 3
+        viewModel = SearchWeatherViewModel(getKeySearchLengthUseCase, searchWeatherUseCase)
     }
 
     @Test
-    fun searchWeather_returnempty() {
+    fun getKeySearchLengthUseCase_return3() {
+        Assert.assertEquals (3, viewModel.minSearchKeyLength.getOrAwaitValue())
+    }
+
+    @Test
+    fun searchWeather_returnEmpty() {
         coEvery { searchWeatherUseCase(any()) } coAnswers {listOf(
-            WeatherElement(1),
-            WeatherElement(2))
+            createWeatherResult(),
+            createWeatherResult())
         }
 
         val keySearch = "ave"
         viewModel.searchWeather(keySearch)
 
-
         coVerify { searchWeatherUseCase(keySearch) }
         Assert.assertEquals (2, viewModel.weatherElement.getOrAwaitValue().size)
     }
+
+    @Test
+    fun viewState_empty() {
+        Assert.assertEquals (CommonViewState.EMPTY, viewModel.viewState.getOrAwaitValue())
+    }
+
+    private fun createWeatherResult(
+        date: Long = 1627272000,
+        averageTemp: Double = 30.0,
+        pressure: Long = 100,
+        humidity: Long = 70,
+        description: String = ""
+    ) = WeatherResultElement(
+        date, averageTemp, pressure, humidity, description
+    )
 }
