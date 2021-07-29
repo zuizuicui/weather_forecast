28a83720fdcf62af3294f7b6a18024cfc6e4faaf
Dai Doan <doanvandai2012@gmail.com>
2021-07-29 15:17:30 +0700

    setup ui test

---
+++ weatherforecast/src/androidTest/java/com/example/weather/weatherforecast/SearchWeather.kt
@@ -0,0 +1,13 @@
+//package com.example.weather.weatherforecast
+//
+//import com.example.weather.domain.interaction.searchweather.SearchWeatherInfoUseCase
+//import dagger.hilt.components.SingletonComponent
+//import dagger.hilt.testing.TestInstallIn
+//
+//@TestInstallIn(
+//    components = [SingletonComponent::class],
+//    replaces = [SearchWeatherInfoUseCase::class]
+//)
+//
+//class SearchWeather {
+//}
--- weatherforecast/src/androidTest/java/com/example/weather/weatherforecast/ui/searchweather/SearchWeatherFragmentTest.kt
+++ weatherforecast/src/androidTest/java/com/example/weather/weatherforecast/ui/searchweather/SearchWeatherFragmentTest.kt
@@ -1,24 +1,34 @@
 package com.example.weather.weatherforecast.ui.searchweather
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
 import com.example.weather.weatherforecast.R
 import com.example.weather.weatherforecast.WeatherForecastActivity
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.mockk
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class SearchWeatherFragmentTest {
-    @Rule
-    @JvmField
-    val activityTestRule = ActivityTestRule(WeatherForecastActivity::class.java)
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @BindValue
+    val mockMyViewModel= mockk<SearchWeatherViewModel>(relaxed = true)
 
     @Test
     fun showUi() {
+        ActivityScenario.launch(WeatherForecastActivity::class.java)
+        onView(withId(R.id.et_search_input)).perform(ViewActions.typeText("hanoi"))
         onView(withId(R.id.btn_get_weather)).perform(ViewActions.click())
+        onView(withId(R.id.loading)).check(matches(isDisplayed()))
     }
 }
